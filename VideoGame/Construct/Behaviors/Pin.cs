48b5e4f620332e3b07b7ed10fa043e76a3a802b0
draqqqula <121135567+draqqqula@users.noreply.github.com>
2023-06-17 00:08:07 +0500

    Добавлен Pin
    IBehavior Pin позволяет присоединить один объект этого поведения к другому объекту GameObject с разностью в позициях Offset

---
+++ VideoGame/Construct/Behaviors/Pin.cs
@@ -0,0 +1,41 @@
+﻿using Animations;
+using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VideoGame
+{
+    public class Pin : IBehavior
+    {
+        public string Name => "Pin";
+
+        public GameObject Parent { get; set; }
+        public bool Enabled { get; set; }
+
+        private GameObject Board;
+        private Vector2 Offset;
+        private Vector2 PinPosition { get => Board is null? Vector2.Zero: Board.Position + Offset; }
+
+        public void Act()
+        {
+            if (Board.ToDestroy) Board = null;
+            if (!(Board is null))
+                Parent.Position = PinPosition;
+        }
+
+        public DrawingParameters ChangeAppearance(DrawingParameters parameters)
+        {
+            return parameters;
+        }
+
+        public Pin(GameObject board, Vector2 offset, bool enabled)
+        {
+            Board = board;
+            Offset = offset;
+            Enabled = enabled;
+        }
+    }
+}
